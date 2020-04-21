fe93c17e919b6a5f9aec2e486ac0e88b30ab1b9f
Troy Clevenger <tclevenger@portalarchitects.com>
2020-04-21 16:14:15 -0400

    Updated to latest dotnet.

--- ApiTier/Startup.cs
+++ ApiTier/Startup.cs
@@ -23,11 +23,13 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(option => option.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseStaticFiles();
             app.UseMvc(routes =>
